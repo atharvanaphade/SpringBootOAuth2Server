235bb31737a40428fca014f320279c67f9138511
Atharva Naphade <naphade21@gmail.com>
2021-11-14 16:58:53 +0530

    Added OAuth2 Client

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Spring Boot OAuth2 Client (Login with GitHub and Google) : -
 
+- This project uses the GitHib authorization server and the inbuilt User in Spring to authenticate users.
+- Here the project is a OAuth2 Client rather than a server.
--- src/main/java/com/test/springoauth/SpringOAuthApplication.java
+++ src/main/java/com/test/springoauth/SpringOAuthApplication.java
@@ -2,14 +2,53 @@ package com.test.springoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-@EnableResourceServer
-public class SpringOAuthApplication {
+@RestController
+public class SpringOAuthApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringOAuthApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(o -> o
+                        .failureHandler((request, response, exception) -> {
+                            request.getSession().setAttribute("error.message", exception.getMessage());
+//                            handler.onAuthenticationFailure(request, response, exception);
+                        })
+                );
     }
 
 }
